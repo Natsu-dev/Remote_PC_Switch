642ce4cae821a0d5cf69da28598bae40736df995
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-11-18 01:00:36 +0900

    spinner実装

--- app/src/main/java/com/example/remotepcswitch/MainActivity.kt
+++ app/src/main/java/com/example/remotepcswitch/MainActivity.kt
@@ -2,26 +2,39 @@ package com.example.remotepcswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    private val listAr = arrayOf("1", "2", "3", "4", "5") //リストの中身
+    private val listAr = arrayOf("0","1", "2", "3", "4", "5") //リストの中身
+    private var select: Int = 0 //リスト選択時の番号
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinner = findViewById<Spinner>(R.id.connSpinner) //connectSpinnerを見つけておく
+        val spinner = findViewById<Spinner>(R.id.connSpinner) //connSpinnerを見つけておく
+        val button = findViewById<Button>(R.id.mainButton) //mainButtonを見つけておく
+
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listAr) //adapterを作成
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) //選択肢の各項目をレイアウト
         spinner.adapter = adapter //spinnerのadapterとして設定
 
-        val button = findViewById<Button>(R.id.mainButton) //mainButtonを見つけておく
-        button.setOnClickListener {
-            Toast.makeText(this, "Button pushed.", Toast.LENGTH_SHORT).show()
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener { //spinnerの中身について
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?, position: Int, id: Long) { //リストの中身が選択された
+                select = spinner.selectedItemPosition //選択された番号をselectに格納
+                Toast.makeText(this@MainActivity, "接続先"+ select + "番が選択されました", Toast.LENGTH_SHORT).show() //格納した旨を表示
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { //何も選択されなかった場合
+              //
+            }
+        }
+
+        button.setOnClickListener { //mainButton押したときの処理
+            Toast.makeText(this, "Button pushed.", Toast.LENGTH_SHORT).show() //押した旨を表示
         }
     }
 
