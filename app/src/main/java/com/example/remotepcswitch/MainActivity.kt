bb385e1fcd71c1908aa72ebd012136dcc045fc7f
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-11-17 16:36:43 +0900

    Design added

--- app/src/main/java/com/example/remotepcswitch/MainActivity.kt
+++ app/src/main/java/com/example/remotepcswitch/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.remotepcswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val listAr = arrayOf("1", "2", "3", "4", "5") //リストの中身
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.connSpinner) //connectSpinnerを見つけておく
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listAr) //adapterを作成
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) //選択肢の各項目をレイアウト
+        spinner.adapter = adapter //spinnerのadapterとして設定
+
+        val button = findViewById<Button>(R.id.mainButton) //mainButtonを見つけておく
+        button.setOnClickListener {
+            Toast.makeText(this, "Button pushed.", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
